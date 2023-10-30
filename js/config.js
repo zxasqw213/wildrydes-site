dd58b8c4b749a830bf877c51fc1d49e9c973b554
Amber <tinytechnicaltutorials@gmail.com>
2023-10-30 01:00:01 +0000

    Edited config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', //'us-west-2_i2LuXLb2z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', //'4fm07m38q7vnq5065vro3n8bhp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_i2LuXLb2z', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4fm07m38q7vnq5065vro3n8bhp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
