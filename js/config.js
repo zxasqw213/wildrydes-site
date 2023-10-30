eccfa922e4e89c22c5d7a691bc27b96152638a3d
Amber <tinytechnicaltutorials@gmail.com>
2023-10-30 00:37:11 +0000

    Edited config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_i2LuXLb2z', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4fm07m38q7vnq5065vro3n8bhp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
